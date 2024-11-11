4020e13f93f81d44159a2a4cb6bab0682ff2b53d
anton-glebov <anton.glebov@useinsider.com>
2024-11-11 16:48:21 +0300

    feat: init track

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { ArtistModule } from './artist/artist.module';
+import { TrackModule } from './track/track.module';
 
 @Module({
   imports: [
@@ -12,6 +13,7 @@ import { ArtistModule } from './artist/artist.module';
     }),
     UserModule,
     ArtistModule,
+    TrackModule,
   ],
   controllers: [AppController],
   providers: [AppService],
