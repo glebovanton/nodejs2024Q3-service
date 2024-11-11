6d30e54d3d3b583d7993c9ca19e82a4f9d22feed
anton-glebov <anton.glebov@useinsider.com>
2024-11-12 01:06:12 +0300

    feat: refactor Welcome message

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import dotenv from 'dotenv';
+
+dotenv.config();
+const { HOST = 'localhost', PORT = '4000' } = process.env;
 
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    const link = `http://${HOST}:${PORT}/doc/`;
+    return `Welcome to Home Library Service! Go to <a href="${link}">${link}</a>`;
   }
 }
