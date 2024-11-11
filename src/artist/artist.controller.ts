1f8914ac76befd4faf80e754989cdba8d879d4f3
anton-glebov <anton.glebov@useinsider.com>
2024-11-11 21:14:27 +0300

    feat: add artist conrtoller service

--- src/artist/artist.controller.ts
+++ src/artist/artist.controller.ts
@@ -1,4 +1,58 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Put,
+  Delete,
+  Param,
+  Body,
+  Header,
+  HttpCode,
+  ParseUUIDPipe,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
+import { Artist } from './entities/artist.entity';
+import { CreateArtistDto } from './dto/create-artist.dto';
+import { UpdateArtistDto } from './dto/update-artist.dto';
+import { ArtistService } from '@/artist/artist.service';
 
 @Controller('artist')
-export class ArtistController {}
+export class ArtistController {
+  constructor(private readonly artistService: ArtistService) {}
+
+  @Get()
+  @Header('Content-Type', 'application/json')
+  findAll(): Artist[] {
+    return this.artistService.findAll();
+  }
+
+  @Get(':id')
+  @Header('Content-Type', 'application/json')
+  findOne(@Param('id', ParseUUIDPipe) id: string): Artist {
+    return this.artistService.findOne(id);
+  }
+
+  @UsePipes(new ValidationPipe())
+  @Post()
+  @Header('Content-Type', 'application/json')
+  create(@Body() dto: CreateArtistDto): Artist {
+    return this.artistService.create(dto);
+  }
+
+  @UsePipes(new ValidationPipe())
+  @Put(':id')
+  @Header('Content-Type', 'application/json')
+  update(
+    @Param('id', ParseUUIDPipe) id: string,
+    @Body() dto: UpdateArtistDto,
+  ): Artist {
+    return this.artistService.update(id, dto);
+  }
+
+  @Delete(':id')
+  @HttpCode(204)
+  delete(@Param('id', ParseUUIDPipe) id: string): void {
+    this.artistService.delete(id);
+  }
+}
--- src/artist/artist.service.ts
+++ src/artist/artist.service.ts
@@ -1,4 +1,72 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { v4 as uuidv4 } from 'uuid';
+import { CreateArtistDto } from './dto/create-artist.dto';
+import { UpdateArtistDto } from './dto/update-artist.dto';
+import { Artist } from './entities/artist.entity';
+import { DatabaseService } from 'src/database/database.service';
 
 @Injectable()
-export class ArtistService {}
+export class ArtistService {
+  constructor(private dbService: DatabaseService) {}
+
+  public findAll(): Artist[] {
+    return this.dbService.artists;
+  }
+
+  public findOne(id: string): Artist {
+    return this.findArtist(id);
+  }
+
+  public create(dto: CreateArtistDto): Artist {
+    const artist: Artist = {
+      id: uuidv4(),
+      ...dto,
+    };
+
+    this.dbService.artists.push(artist);
+
+    return artist;
+  }
+
+  public update(id: string, dto: UpdateArtistDto): Artist {
+    const artist = this.findArtist(id);
+
+    return Object.assign(artist, dto);
+  }
+
+  public delete(id: string): void {
+    const artist = this.findArtist(id);
+    const artistIndex = this.dbService.artists.indexOf(artist);
+
+    this.dbService.artists.splice(artistIndex, 1);
+    this.dbService.tracks
+      .filter((track) => track.artistId === artist.id)
+      .forEach((track) => (track.artistId = null));
+    this.dbService.albums
+      .filter((album) => album.artistId === artist.id)
+      .forEach((album) => (album.artistId = null));
+
+    const artistInFavorites: Artist | undefined =
+      this.dbService.favs.artists.find(
+        (artist: Artist): boolean => artist.id === id,
+      );
+
+    if (artistInFavorites) {
+      const artistIndex: number =
+        this.dbService.favs.artists.indexOf(artistInFavorites);
+      this.dbService.favs.artists.splice(artistIndex, 1);
+    }
+  }
+
+  private findArtist(id: string): Artist {
+    const artist: Artist | undefined = this.dbService.artists.find(
+      (artist: Artist): boolean => artist.id === id,
+    );
+
+    if (!artist) {
+      throw new NotFoundException('Artist with this ID not found');
+    }
+
+    return artist;
+  }
+}
