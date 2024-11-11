d6b199f7a26a70f92590cc619bdcbafb3c9e1b65
anton-glebov <anton.glebov@useinsider.com>
2024-11-11 21:03:52 +0300

    feat: add track service conrtoller

--- src/track/track.controller.ts
+++ src/track/track.controller.ts
@@ -1,4 +1,58 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Put,
+  Delete,
+  Param,
+  Body,
+  Header,
+  HttpCode,
+  UsePipes,
+  ValidationPipe,
+  ParseUUIDPipe,
+} from '@nestjs/common';
+import { Track } from './entities/track.entity';
+import { TrackService } from './track.service';
+import { CreateTrackDto } from './dto/create-track.dto';
+import { UpdateTrackDto } from './dto/update-track.dto';
 
 @Controller('track')
-export class TrackController {}
+export class TrackController {
+  constructor(private readonly trackService: TrackService) {}
+
+  @Get()
+  @Header('Content-Type', 'application/json')
+  findAll(): Track[] {
+    return this.trackService.findAll();
+  }
+
+  @Get(':id')
+  @Header('Content-Type', 'application/json')
+  findOne(@Param('id', ParseUUIDPipe) id: string): Track {
+    return this.trackService.findOne(id);
+  }
+
+  @UsePipes(new ValidationPipe())
+  @Post()
+  @Header('Content-Type', 'application/json')
+  create(@Body() dto: CreateTrackDto): Track {
+    return this.trackService.create(dto);
+  }
+
+  @UsePipes(new ValidationPipe())
+  @Put(':id')
+  @Header('Content-Type', 'application/json')
+  update(
+    @Param('id', ParseUUIDPipe) id: string,
+    @Body() dto: UpdateTrackDto,
+  ): Track {
+    return this.trackService.update(id, dto);
+  }
+
+  @Delete(':id')
+  @HttpCode(204)
+  delete(@Param('id', ParseUUIDPipe) id: string): void {
+    this.trackService.delete(id);
+  }
+}
