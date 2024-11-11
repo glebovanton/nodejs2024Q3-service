50c8b015770e2614611c62f900203c693b7c18cf
anton-glebov <anton.glebov@useinsider.com>
2024-11-11 16:42:03 +0300

    feat: init swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,27 @@
+import { readFileSync } from 'node:fs';
+import * as yaml from 'js-yaml';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { SwaggerModule, OpenAPIObject } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
+  const host = configService.get<number>('HOST') || 'localhost';
+  const port = configService.get<number>('PORT') || 4000;
+  const swaggerPath = 'doc';
+
+  const swaggerDoc = yaml.load(
+    readFileSync('doc/api.yaml', { encoding: 'utf-8' }),
+  ) as OpenAPIObject;
+
+  SwaggerModule.setup(swaggerPath, app, swaggerDoc);
+
   await app.listen(4000);
+  console.log(
+    `Swagger ${process.pid} works on the http://${host}:${port}/${swaggerPath}/`,
+  );
 }
+
 bootstrap();
