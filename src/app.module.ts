873d32ea26c6897ad013ffd875958463df65b96b
anton-glebov <anton.glebov@useinsider.com>
2024-11-25 23:34:35 +0300

    feat: add logger

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module, MiddlewareConsumer, NestModule } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { APP_FILTER } from '@nestjs/core';
+import { APP_FILTER, APP_GUARD } from '@nestjs/core';
 import { AppController } from '@/app.controller';
+import { AppService } from '@/app.service';
 import { UserModule } from '@/user/user.module';
 import { ArtistModule } from '@/artist/artist.module';
 import { TrackModule } from '@/track/track.module';
@@ -12,6 +13,7 @@ import { AuthModule } from '@/auth/auth.module';
 import { HttpExceptionFilter } from '@/filter/exception.filter';
 import { LoggerModule } from '@/logger/logger.module';
 import { LoggerMiddleware } from '@/logger/logger.middleware';
+import { AuthGuard } from './auth/auth.guard';
 
 @Module({
   imports: [
@@ -29,6 +31,8 @@ import { LoggerMiddleware } from '@/logger/logger.middleware';
   ],
   controllers: [AppController],
   providers: [
+    AppService,
+    { provide: APP_GUARD, useClass: AuthGuard },
     {
       provide: APP_FILTER,
       useClass: HttpExceptionFilter,
@@ -40,4 +44,3 @@ export class AppModule implements NestModule {
     consumer.apply(LoggerMiddleware).forRoutes('*');
   }
 }
-
--- src/logger/logger.middleware.ts
+++ src/logger/logger.middleware.ts
@@ -1,9 +1,10 @@
-import { Injectable, NestMiddleware, Logger, HttpStatus } from '@nestjs/common';
+import { Injectable, NestMiddleware, HttpStatus } from '@nestjs/common';
 import { NextFunction, Request, Response } from 'express';
+import { LoggerService } from './logger.service';
 
 @Injectable()
 export class LoggerMiddleware implements NestMiddleware {
-  private logger = new Logger(LoggerMiddleware.name);
+  constructor(private readonly logger: LoggerService) {}
 
   use(req: Request, res: Response, next: NextFunction) {
     const { query, method, body, originalUrl } = req;
@@ -17,11 +18,11 @@ export class LoggerMiddleware implements NestMiddleware {
         res.statusCode < HttpStatus.INTERNAL_SERVER_ERROR &&
         res.statusCode >= HttpStatus.BAD_REQUEST
       ) {
-        this.logger.warn(message);
+        this.logger.warn(message, LoggerMiddleware.name);
       } else if (res.statusCode >= HttpStatus.INTERNAL_SERVER_ERROR) {
-        this.logger.error(message);
+        this.logger.error(message, LoggerMiddleware.name);
       } else {
-        this.logger.log(message);
+        this.logger.log(message, LoggerMiddleware.name);
       }
     });
 
