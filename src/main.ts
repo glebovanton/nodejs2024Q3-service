eed78185412a712f20798110bb5687c9ece4903b
anton-glebov <anton.glebov@useinsider.com>
2024-11-25 19:34:55 +0300

    feat: add main logger and filter

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,44 @@
 import { readFileSync } from 'node:fs';
+import * as dotenv from 'dotenv';
 import * as yaml from 'js-yaml';
-import { ValidationPipe } from '@nestjs/common';
-import { NestFactory } from '@nestjs/core';
+import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ConfigService } from '@nestjs/config';
 import { SwaggerModule, OpenAPIObject } from '@nestjs/swagger';
+import { LoggerService } from '@/logger/logger.service';
+import { HttpExceptionFilter } from '@/filter/exception.filter';
+
+dotenv.config();
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  const configService = app.get(ConfigService);
-  const host = configService.get<number>('HOST') || 'localhost';
-  const port = configService.get<number>('PORT') || 4000;
+  const app = await NestFactory.create(AppModule, {
+    logger: new LoggerService(),
+  });
+  const host = process.env.HOST || 'localhost';
+  const port = process.env.PORT || 4000;
   const swaggerPath = 'doc';
+  const logger: LoggerService = app.get(LoggerService);
+  const httpAdapterHost = app.get(HttpAdapterHost);
 
   const swaggerDoc = yaml.load(
     readFileSync('doc/api.yaml', { encoding: 'utf-8' }),
   ) as OpenAPIObject;
 
+  app.useLogger(logger);
+  app.useGlobalFilters(new HttpExceptionFilter(httpAdapterHost));
+
   SwaggerModule.setup(swaggerPath, app, swaggerDoc);
-  app.useGlobalPipes(new ValidationPipe());
+
+  process.on('unhandledRejection', (reason) => {
+    logger.error(
+      'Unhandled Rejection!',
+      reason instanceof Error ? reason.stack : String(reason),
+    );
+  });
+  process.on('uncaughtException', (e: Error) => {
+    logger.error('Uncaught Exception!', e.stack);
+    process.exit(1);
+  });
+
   await app.listen(port);
   console.log(
     `Swagger ${process.pid} works on the http://${host}:${port}/${swaggerPath}/`,
