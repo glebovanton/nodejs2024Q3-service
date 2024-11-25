cd7b290ac2ad1826fef8976787197a627616ecaf
anton-glebov <anton.glebov@useinsider.com>
2024-11-26 02:51:24 +0300

    feat: fix LoggerMiddleware test

--- src/logger/logger.middleware.spec.ts
+++ src/logger/logger.middleware.spec.ts
@@ -1,7 +1,9 @@
 import { LoggerMiddleware } from './logger.middleware';
+import { LoggerService } from '@/logger/logger.service';
 
 describe('LoggerMiddleware', () => {
   it('should be defined', () => {
-    expect(new LoggerMiddleware()).toBeDefined();
+    const mockLoggerService: LoggerService = new LoggerService();
+    expect(new LoggerMiddleware(mockLoggerService)).toBeDefined();
   });
 });
